2f3aadb297f319a74de568339b91884c74800ec9
Harley Macbook <harley@baffleinc.com>
2015-10-04 17:41:11 +1100

    last few changes hopefully

--- wp-content/themes/treehouse/builder/builder-3-cols.php
+++ wp-content/themes/treehouse/builder/builder-3-cols.php
@@ -9,13 +9,13 @@
 <section class="columns-3" id="process">
 	<div class="row">
 
-		<h1 class="text-center">Our Process</h1>
-
+		
+		<h1 class="text-center"><?php the_sub_field('3-col-title') ?></h1>
 		<?php if(have_rows('columns')) : while(have_rows('columns')) : the_row(); ?>
-
+			
 			<div class="small-12 medium-4 columns text-center" fade-into-view>
-				<h2><?php the_sub_field('title') ?></h2>
 				<img class="icon" src="<?php the_sub_field('icon') ?>" alt="">
+				<h2><?php the_sub_field('title') ?></h2>
 				<p><?php the_sub_field('subtitle') ?></p>
 			</div>
 
